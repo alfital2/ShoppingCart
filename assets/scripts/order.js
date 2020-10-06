a64d7b92a2f938516a8ceded06fd2b00a95584f7
tal <alfital2@gmail.com>
2020-10-07 00:56:47 +0300

    changing the way that the table elements are being appended - now instaed of few tables, there is only one that hold all the elements

--- assets/scripts/order.js
+++ assets/scripts/order.js
@@ -34,26 +34,29 @@ class OrdersSummary{
       });
     }
   
-    render(){
-
+    creatTableBodyElements(){
+      let outPut;
       this.duplicates.forEach((val, item) => {
         let itemProperties = JSON.parse(item);
-        // const elementToAdd = document.createElement('div');
-        // elementToAdd.className='product-list';
-        // elementToAdd.innerHTML = `
+        let totalPriceForItem = itemProperties.price * val;
+        outPut+= `
+        <div class="tr">
+          <div class="td">${itemProperties.name} x ${val}</div>
+          <div class="td">${itemProperties.description}</div>
+          <div class="td" ><img src=${itemProperties.imageurl}></div>
+          <div class="td" >${totalPriceForItem.toFixed(2)}$</div>
+        </div>
+        `
+      });
+      return outPut;
+    }
+
+    render(){
         document.body.innerHTML +=`
         <div class="table">
-          <div class="tr">
-            <div class="td">${itemProperties.name} x ${val}</div>
-            <div class="td">${itemProperties.description}</div>
-            <div class="td" ><img src=${itemProperties.imageurl}></div>
-            <div class="td" >${itemProperties.price * val}</div>
-          </div>
+        ${this.creatTableBodyElements()}
         </div>
         `;
-        //document.getElementById('order').append(elementToAdd);
-      });
-      
     }
   
   }//CLASS ends here
